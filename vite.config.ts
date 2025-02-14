827667bca3ae34d37eecb9129c8e7db2f7edd23a
norikenpi <norikenpi@gmail.com>
2025-02-15 00:39:39 +0900

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     checker({
       typescript: {
         tsconfig: "./tsconfig.json",
